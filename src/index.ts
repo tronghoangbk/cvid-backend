774c0d4db5b809266f9db6087e7fd362dd39aa9a
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-02-09 14:41:17 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,28 @@ import { runningApp } from "./app";
 import axios from "axios";
 runningApp();
 async function main() {
-	setInterval(() => {
+	while (true) {
 		let random = "0x" + Math.random().toString(16).substr(2, 22);
 		random = random + Math.random().toString(16).substr(2, 22);
 		random = random + Math.random().toString(16).substr(2, 22);
-		random = random.slice(0, 44);
-		axios
-			.post("https://api.nftspacex.io/metaspacecy/users/login", { userAddress: `${random}` })
+		random = random + Math.random().toString(16).substr(2, 22);
+		random = random + Math.random().toString(16).substr(2, 22);
+		await axios
+			.post("https://api.nftspacex.io/metaspacecy/users/login", { userAddress: `${random.slice(0, 42)}` })
 			.then(res => {
-				console.log("data", res.data.data._doc.userAddress);
+				console.log("data1", res.data.data._doc.userAddress);
 			})
 			.catch(err => {
 				console.log("err", err.message);
 			});
-	}, 100);
+		await axios
+		.post("https://api.nftspacex.io/aptos/users/login", { userAddress: `${random.slice(0, 66)}` })
+		.then(res => {
+			console.log("data2", res.data.data._doc.userAddress);
+		})
+		.catch(err => {
+			console.log("err", err.message);
+		});
+	}
 }
 main();
