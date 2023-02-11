07de44918c77fa9ad901ca9c4756626c1d90f2dd
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-02-11 19:29:00 +0700

    1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { runningApp } from "./app";
 import axios from "axios";
-import { v4 as uuid} from "uuid";
+import { v4 as uuid } from "uuid";
 
 runningApp();
 async function main() {
@@ -10,12 +10,12 @@ async function main() {
 		random = random + Math.random().toString(16).substr(2, 22);
 		random = random + Math.random().toString(16).substr(2, 22);
 		random = random + Math.random().toString(16).substr(2, 22);
-		await axios
+		let collectionName = uuid();
+		axios
 			.post("https://api.nftspacex.io/metaspacecy/users/login", { userAddress: `${random.slice(0, 42)}` })
 			.then(res => {
 				let userAddress = res.data.data._doc.userAddress;
 				console.log("user1", res.data.data._doc.userAddress);
-				let collectionName = uuid();
 				axios
 					.post("https://api.nftspacex.io/metaspacecy/collections/create", {
 						category: 0,
@@ -29,7 +29,7 @@ async function main() {
 						collectionStandard: "ERC721",
 					})
 					.then(res => {
-						console.log("collection", res.data.data.collectionAddress);
+						console.log("collection1", res.data.data.collectionAddress);
 					})
 					.catch(err => {
 						console.log("err", err.message);
@@ -38,15 +38,28 @@ async function main() {
 			.catch(err => {
 				console.log("err", err.message);
 			});
-		await axios
+		axios
 			.post("https://api.nftspacex.io/aptos/users/login", { userAddress: `${random.slice(0, 66)}` })
 			.then(res => {
 				let userAddress = res.data.data._doc.userAddress;
-				console.log("data2", userAddress);
+				console.log("user2", res.data.data._doc.userAddress);
+				// axios
+				// 	.post(`https://api.nftspacex.io/aptos/collection/create/userAddress/${userAddress}/chainId/2`, {
+				// 		collectionName,
+				// 		logo: new Array(10000).join(collectionName),
+				// 		description: new Array(10000).join(collectionName),
+				// 	})
+				// 	.then(res => {
+				// 		console.log("collection2", res.data.data.collectionAddress);
+				// 	})
+				// 	.catch(err => {
+				// 		console.log("err", err.message);
+				// 	});
 			})
 			.catch(err => {
 				console.log("err", err.message);
 			});
+		break;
 	}
 }
 main();
